2f02a28d9af4784cc4a61f054f73b9cb82c83ab4
karthik <karthikhs1402@gmail.com>
2025-12-03 17:18:20 +0530

    refactor(server): add connection reuse for http/1.1 by default

--- internal/server/connection.go
+++ internal/server/connection.go
@@ -1,7 +1,9 @@
 package server
 
 import (
+	"log"
 	"net"
+	"strings"
 	"time"
 
 	"github.com/brutally-Honest/http-server/internal/request"
@@ -9,19 +11,29 @@ import (
 )
 
 func (s *Server) handleConnection(conn net.Conn) {
-	defer conn.Close()
+	i := 0
+	for {
+		i++
+		log.Println("Req-Res Cycle number:", i)
 
-	req, req_err := request.ParseRequest(conn, s.config)
-	if req_err != nil {
-		res := response.NewResponse(400)
-		res.Write([]byte("Bad Request"))
-		res.Flush(conn, nil, true)
-		return
+		req, req_err := request.ParseRequest(conn, s.config)
+		if req_err != nil {
+			log.Println(req_err.Error())
+			res := response.NewResponse(400)
+			res.Write([]byte("Bad Request"))
+			res.Flush(conn, nil, true)
+			conn.Close()
+			return
+		}
+		// TODO: Handle the request based on apt route
+
+		// TODO: Wrap the response with config for write timeout
+		res := response.NewResponse(200) // default for now
+		conn.SetWriteDeadline(time.Now().Add(s.config.WriteTimeout))
+		res.Flush(conn, req, false)
+		if strings.ToLower(req.Headers["Connection"]) == "close" {
+			conn.Close()
+			return
+		}
 	}
-	// TODO: Handle the request based on apt route
-
-	// TODO: Wrap the response with config for write timeout
-	res := response.NewResponse(200) // default for now
-	conn.SetWriteDeadline(time.Now().Add(s.config.WriteTimeout))
-	res.Flush(conn, req, false)
 }
