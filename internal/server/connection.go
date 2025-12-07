e9557832175235336011ba3e7ab277e1faba3de2
karthik <karthikhs1402@gmail.com>
2025-12-07 06:02:19 +0530

    refactor(connection): add context for connection

--- internal/server/connection.go
+++ internal/server/connection.go
@@ -1,6 +1,7 @@
 package server
 
 import (
+	"context"
 	"log"
 	"net"
 	"strings"
@@ -17,27 +18,30 @@ func (s *Server) handleConnection(conn net.Conn) {
 			conn.Close()
 		}
 	}()
-	i := 0
-	for {
-		i++
-		log.Println("Req-Res Cycle number:", i)
+	ctx, cancelConn := context.WithCancel(context.Background())
+	defer cancelConn()
 
+	for {
 		req, reqErr := request.ParseRequest(conn, s.config)
 		if reqErr != nil {
 			log.Println(reqErr.Error())
+			cancelConn()
 			res := response.NewResponse(400)
 			res.Write([]byte("Bad Request"))
 			res.Flush(conn, nil, true)
 			conn.Close()
 			return
 		}
-		// TODO: Handle the request based on apt route
+		_, cancelReq := context.WithCancel(ctx)
+		// TODO: Handle the request based on apt route with reqCtx passed
 
 		// TODO: Wrap the response with config for write timeout
 		res := response.NewResponse(200) // default for now
 		conn.SetWriteDeadline(time.Now().Add(s.config.WriteTimeout))
 		res.Flush(conn, req, false)
+		cancelReq()
 		if strings.ToLower(req.Headers["Connection"]) == "close" {
+			cancelConn()
 			conn.Close()
 			return
 		}
